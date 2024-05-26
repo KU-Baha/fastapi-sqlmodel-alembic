87a84c6a387719a8b06350035c8e0fc961ce098e
ku-baha <kulbarakovbh@gmail.com>
2024-05-26 14:14:39 +0600

    Add endpoints for songs
    Summary
    * Add enpoint create song "/songs" POST
    * Add enpoint get list gons "/songs" GET
    
    Test plan
    * Go to localhost:8000/docs/
    * Send request to "/songs" POST
    * Check created song exists in "/songs" GET

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-from app.db import init_db
-from app.models import Song # noqa
+from sqlmodel import Session, select
+from app.db import init_db, get_session
+from app.models import Song, SongCreate, SongBase  # noqa
 
 app = FastAPI()
 
@@ -16,3 +17,27 @@ async def pong():
     return {"ping": "pong!"}
 
 
+@app.get("/songs", response_model=list[Song])
+def get_songs(session: Session = Depends(get_session)):
+    result = session.execute(select(Song))
+    songs: [Song] = result.scalars().all()
+    return [
+        Song(
+            id=song.id,
+            name=song.name,
+            artist=song.artist
+        )
+        for song in songs
+    ]
+
+
+@app.post("/songs")
+def add_song(song: SongBase, session: Session = Depends(get_session)):
+    song = Song(
+        name=song.name,
+        artist=song.artist
+    )
+    session.add(song)
+    session.commit()
+    session.refresh(song)
+    return song
