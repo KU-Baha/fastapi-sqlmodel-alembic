e2e36dcb93ae2b9fefe762bcc16b44df264a8449
ku-baha <kulbarakovbh@gmail.com>
2024-05-26 13:51:36 +0600

    Create DB models and update folder architecture. Add poetry.
    Summary
    * Create tables for Song model.
    * Update requirements for mac (poetry) and for windows.
    * Update .gitignore file
    * docker compose file update, change port from 8004 to 8000
    * Change project folder to src folder
    * Dockerfile update requirements folder
    
    Test plan
    * Run docker compose
    * Check web logs, checking created tables with command "docker-compose logs web"
    * Open psql with command "docker-compose exec db psql --username=postgres --dbname=foo" and check tables with command "\dt"

---
+++ src/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+from app.db import init_db
+from app.models import Song # noqa
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong!"}
+
+
